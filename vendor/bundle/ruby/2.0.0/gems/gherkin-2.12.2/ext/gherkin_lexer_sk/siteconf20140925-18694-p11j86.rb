5022d7087e717bfd41c924659a37541f9f344cd1
satoshirato <satoshirato@gmail.com>
2014-09-25 08:52:56 +0900

    initial commit

---
+++ vendor/bundle/ruby/2.0.0/gems/gherkin-2.12.2/ext/gherkin_lexer_sk/siteconf20140925-18694-p11j86.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20140925-18694-1g3b6n4"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
---
+++ vendor/bundle/ruby/2.0.0/gems/sqlite3-1.3.9/ext/sqlite3/siteconf20140924-75916-1x0hc1y.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20140924-75916-12gqwxg"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
